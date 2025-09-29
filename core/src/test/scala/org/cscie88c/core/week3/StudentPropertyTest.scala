96ca08e789155ed4dadddb39e110716b9aeae38e
qwong95 <qwong1995@gmail.com>
2025-09-29 16:41:51 -0400

    Changes for HW3

--- core/src/test/scala/org/cscie88c/core/week3/StudentPropertyTest.scala
+++ core/src/test/scala/org/cscie88c/core/week3/StudentPropertyTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import org.scalacheck._
 import org.cscie88c.core.week3.Student.averageScoreBySubject
+import org.cscie88c.core.week3.Student.averageScoreBySubject
 
 class StudentPropertyTest
     extends AnyFunSuite
@@ -29,35 +30,32 @@ class StudentPropertyTest
   // complete the student list generator below if attempting bonus problem
   val studentsListGen: Gen[List[Student]] = for {
     n <- Gen.choose(0, 30)
-    list <- Gen.listOfN(
-      n,
-      for {
-        name <- nameGen
-        user <- userGen
-        domain <- domainGen
-        subject <- subjectGen
-        score <- subject match {
-          case "Math" => scoreLt100
-          case _      => scoreGen
-        }
-      } yield Student(name, s"$user@$domain", subject, score)
-    )
+    list <- Gen.listOfN(n, for {
+      name    <- nameGen
+      user    <- userGen
+      domain  <- domainGen
+      subject <- subjectGen
+      score   <- subject match {
+        case "Math"    => scoreLt100
+        case _         => scoreGen
+      }
+    } yield Student(name, s"$user@$domain", subject, score))
   } yield list
 
   test("description contains name and email") {
     forAll(studentGen) { student =>
-      val desc = student.description
-      withClue(s"Description [$desc] should contain name and email") {
-        desc should include(student.name)
-        desc should include(student.email)
-      }
+        val desc = student.description
+        withClue(s"Description [$desc] should contain name and email") {
+          desc should include(student.name)
+          desc should include(student.email)
+        }
     }
   }
 
   test("averageScoreBySubject (Math) < 100 for generated student lists") {
-    forAll(studentsListGen) { students =>
-      val avgMath = averageScoreBySubject("Math", students)
-      avgMath < 100.0 shouldBe true
-    }
+      forAll(studentsListGen) { students =>
+        val avgMath = averageScoreBySubject("Math", students)
+        avgMath < 100.0 shouldBe true
+      }
   }
 }
