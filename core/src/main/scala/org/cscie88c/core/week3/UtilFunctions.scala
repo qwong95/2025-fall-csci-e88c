9b23c3910b4211683a8f15d8c93b5e4526bb9dd0
qwong95 <qwong1995@gmail.com>
2025-09-29 22:03:54 -0400

    I have no idea what I'm doing

--- core/src/main/scala/org/cscie88c/core/week3/UtilFunctions.scala
+++ core/src/main/scala/org/cscie88c/core/week3/UtilFunctions.scala
@@ -7,9 +7,9 @@ object UtilFunctions {
   def pythTest(x: Int, y: Int, z: Int): Boolean = x * x + y * y == z * z
 
   def pythTriplesUpto100: List[(Int, Int, Int)] = (for {
-      x <- 1 to 100
-      y <- x to 100
-      z <- y to 100
-      if x * x + y * y == z * z
-    } yield (x, y, z)).toList
+    x <- 1 to 100
+    y <- x to 100
+    z <- y to 100
+    if x * x + y * y == z * z
+  } yield (x, y, z)).toList
 }
