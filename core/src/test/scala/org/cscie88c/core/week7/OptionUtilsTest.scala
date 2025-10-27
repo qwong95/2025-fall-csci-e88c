0118cef4cb1196765bb752393064a756bc38e212
qwong95 <qwong1995@gmail.com>
2025-10-27 09:47:00 -0400

    Changes for HW7 1st problem

--- core/src/test/scala/org/cscie88c/core/week7/OptionUtilsTest.scala
+++ core/src/test/scala/org/cscie88c/core/week7/OptionUtilsTest.scala
@@ -4,10 +4,25 @@ import org.cscie88c.core.testutils.{ StandardTest }
 import scala.util.{Try, Success, Failure}
 
 class OptionUtilsTest extends StandardTest {
+  import OptionUtils._
+
+  private val existingFile = "data/dirty-retail-data-sample.csv"
+  private val missingFile = "data/dirty-retail-data-sample.csv-x"
+
   "OptionUtils" when {
     "calling fileCharCount" should {
       "return the correct number of characters in a valid file" in {
-        // add unit tests below
+        fileCharCount(existingFile) shouldBe Success(195L)
+      }
+    }
+
+    "calling charCountAsString" should {
+      "return the correct count string for the existing file" in {
+        charCountAsString(existingFile) shouldBe "number of characters: 195"
+      }
+
+      "return the error message for a missing file" in {
+        charCountAsString(missingFile) shouldBe "error opening file"
       }
     }
   }
